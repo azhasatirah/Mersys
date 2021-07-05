bbf98da44e7e9d1cfaa6b07a9add4df139d70537
sssware <ahmad.yusup0170@gmail.com>
2021-07-05 14:59:06 +0700

    update fotn sertifikat depan

--- resources/views/karyawan/sertifikat/depan.blade.php
+++ resources/views/karyawan/sertifikat/depan.blade.php
@@ -8,6 +8,14 @@
 
     <!-- Bootstrap CSS -->
     <style>
+        @font-face { 
+     
+            font-family:comic; src: url('/fonts/comici.ttf');
+        }
+        @font-face { 
+     
+            font-family:play; src: url('/fonts/p3.otf');
+        }
         #body {
             background-image:url('{{url('images/sertifikat/s-depan-blank.png')}}');
             background-repeat: no-repeat;
@@ -58,10 +66,10 @@
     @endif
     <div id="body" class="container-fluid">
         <div class="d-flex justify-content-between">
-            <div></div>
+            <div ></div>
             <div id="content" style="padding-top: 280px;padding-left:225px">
-                <h4 id="nama" >{{$Nilai['NamaSiswa']}}</h4>
-                <p id="des" style="color: red;font-size:14px">
+                <h1 style="font-family: play" id="nama" >{{$Nilai['NamaSiswa']}}</h1>
+                <p id="des" style="color: red;font-size:14px;font-family:comic">
                     has successfully completed the Merachel Program: 
                     <span style="text-decoration: underline">{{$Nilai['NamaProdi']}}</span> <br>
                     demonstrating the required skills and knowledge. The Final Evaluation was passed <br>
--- resources/views/karyawan/sertifikat/print_depan.blade.php
+++ resources/views/karyawan/sertifikat/print_depan.blade.php
@@ -8,6 +8,12 @@
 
     <!-- Bootstrap CSS -->
     <style>
+        @font-face { 
+            font-family:comic; src: url('/fonts/comici.ttf');
+        }
+        @font-face { 
+            font-family:play; src: url('/fonts/p3.otf');
+        }
         #body {
             background-image:url('{{url('images/sertifikat/s-depan-blank.png')}}');
             background-repeat: no-repeat;
@@ -50,9 +56,9 @@
     <div id="body" class="container-fluid">
         <div class="d-flex justify-content-between">
             <div></div>
-            <div id="content" style="padding-top: 280px;padding-left:225px">
-                <h4 id="nama" >{{$Nilai['NamaSiswa']}}</h4>
-                <p id="des" style="color: red;font-size:14px">
+            <div id="content" style="padding-top: 280px;padding-left:225px" >
+                <h4 id="nama" tyle="font-family: play" >{{$Nilai['NamaSiswa']}}</h4>
+                <p id="des" style="color: red;font-size:14px;font-family:comic">
                     has successfully completed the Merachel Program: 
                     <span style="text-decoration: underline">{{$Nilai['NamaProdi']}}</span> <br>
                     demonstrating the required skills and knowledge. The Final Evaluation was passed <br>
