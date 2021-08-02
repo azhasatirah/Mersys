3b4f6cb1d691bc2bb385605b00ce935f57ad5e84
sssware <ahmad.yusup0170@gmail.com>
2021-08-02 17:56:34 +0700

    update data

--- resources/views/karyawan/kasbank.blade.php
+++ resources/views/karyawan/kasbank.blade.php
@@ -205,7 +205,10 @@
                     Total,
                     numberToIDR(ele.PrevTotal+ele.Total),
                     ele.Keterangan,
-                    btn
+                    PrefixKode=='SBK'?btn:
+                    "<button type=\"button\" onclick=\"deleteKasBank("+ele.IDKasBank+")\" class=\"btn btn-primary btn-sm btn-danger\" >"+
+                    "<i class=\"fa fa-trash\"></i>"+
+                    "</button>"+
                 ]).draw()
                 i++
             })
