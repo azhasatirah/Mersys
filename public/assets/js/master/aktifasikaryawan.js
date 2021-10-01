42e511243e97be49d5f8a968c069ecd3692680de
sssware <ahmad.yusup0170@gmail.com>
2021-10-01 17:03:10 +0700

    update pendaftaran karyawan

--- public/assets/js/master/aktifasikaryawan.js
+++ public/assets/js/master/aktifasikaryawan.js
@@ -1,100 +1,64 @@
 var TabelData = $('#tabeldata').DataTable();
 var Aktifasi;
 let role = $('#role').val()==1?'owner':'admin'
+let LocalData = []
+console.log(role)
 $(document).ready(function(){
     $('#tabeldata').DataTable();
-    showData();
+    getData();
 });
 
-function showData(){
-    console.log('hi')
-    $.ajax({
-        type: "get",
-        url: '/karyawan/pendaftaran/karyawan/getdata',
-        success: function (Data) {
-            console.log(Data,Data.Status)
-            if(Data.Status=='success'){
-                $('#datatabel').empty();
-                var a=0;
-                TabelData.clear().draw();
-                Data['AkunKaryawan'].forEach((data) =>{
-                    console.log(data)
-                    var TombolAksi = "<a class=\"btn btn-primary btn-sm\"onClick=\"confirmKaryawan(\'"+data.IDKaryawan+"\')\">"+
-                                    "<i class=\"fa fa-check\"></i></a>";
-                    if(Data.Status === 'OPN' && role === 'admin'){
-                        a++;
-                        TabelData.row.add([
-                            a,
-                            data.NamaKaryawan,
-                            data.TanggalLahir,
-                            data.TempatLahir,
-                            data.JenisKelamin,
-                            data.Alamat,
-                            data.NoHP,
-                            TombolAksi
-                        ]).draw();
-                    }
-                    if(Data.Status === 'CFM' && role === 'owner'){
-                        a++;
-                        TabelData.row.add([
-                            a,
-                            data.NamaKaryawan,
-                            data.TanggalLahir,
-                            data.TempatLahir,
-                            data.JenisKelamin,
-                            data.Alamat,
-                            data.NoHP,
-                            TombolAksi
-                        ]).draw();
-                    }
-                })
-            }
+function getData(){
+    $.get('/karyawan/pendaftaran/karyawan/getdata').done((Data)=>{
+        console.log(Data,Data.Status)
+        if(Data.Status=='success'){
+            LocalData = Data['AkunKaryawan']
+            console.log(LocalData)
+            showData()
         }
-    });
-    // $.get('/karyawan/pendaftaran/karyawan/getdata').done(Data=>{
-    //     console.log(Data,Data.Status)
-    //     if(Data.Status=='success'){
-    //         $('#datatabel').empty();
-    //         var a=0;
-    //         TabelData.clear().draw();
-    //         Data['AkunKaryawan'].forEach((data) =>{
-    //             console.log(data)
-    //             var TombolAksi = "<a class=\"btn btn-primary btn-sm\"onClick=\"confirmKaryawan(\'"+data.IDKaryawan+"\')\">"+
-    //                             "<i class=\"fa fa-check\"></i></a>";
-    //             if(Data.Status === 'OPN' && role === 'admin'){
-    //                 a++;
-    //                 TabelData.row.add([
-    //                     a,
-    //                     data.NamaKaryawan,
-    //                     data.TanggalLahir,
-    //                     data.TempatLahir,
-    //                     data.JenisKelamin,
-    //                     data.Alamat,
-    //                     data.NoHP,
-    //                     TombolAksi
-    //                 ]).draw();
-    //             }
-    //             if(Data.Status === 'CFM' && role === 'owner'){
-    //                 a++;
-    //                 TabelData.row.add([
-    //                     a,
-    //                     data.NamaKaryawan,
-    //                     data.TanggalLahir,
-    //                     data.TempatLahir,
-    //                     data.JenisKelamin,
-    //                     data.Alamat,
-    //                     data.NoHP,
-    //                     TombolAksi
-    //                 ]).draw();
-    //             }
-    //         })
-    //     }
-    // })
+    })
+}
+function showData(){
+    var a=0;
+    TabelData.clear().draw();
+    LocalData.forEach((data) =>{
+        console.log('loop all data')
+        var TombolAksi = "<a class=\"btn btn-primary btn-sm\"onClick=\"confirmKaryawan(\'"+data.IDKaryawan+"\')\">"+
+                        "<i class=\"fa fa-check\"></i></a>";
+        if(Data.Status === 'OPN' && role === 'admin'){
+            console.log(data)
+            a++;
+            TabelData.row.add([
+                a,
+                data.NamaKaryawan,
+                data.TanggalLahir,
+                data.TempatLahir,
+                data.JenisKelamin,
+                data.Alamat,
+                data.NoHP,
+                TombolAksi
+            ]).draw();
+        }
+        if(Data.Status === 'CFM' && role === 'owner'){
+            console.log(data)
+            a++;
+            TabelData.row.add([
+                a,
+                data.NamaKaryawan,
+                data.TanggalLahir,
+                data.TempatLahir,
+                data.JenisKelamin,
+                data.Alamat,
+                data.NoHP,
+                TombolAksi
+            ]).draw();
+        }
+    })
 }
 function confirmKaryawan(id){
     $.get('/karyawan/'+role+'pendaftaran/karyawan/'+id,res=>{
         swal(res)
-        showData()
+        getData()
     })
 }
 function storeData(){
